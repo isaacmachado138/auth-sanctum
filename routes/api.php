41013d58e9089df916d183231fbe4b20a02c9a17
isaac.machado <isaac.machado@35.168.70.227>
2024-11-06 16:30:30 -0300

    fix: comment routes and fix route verify, calling middleware for verification only

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,14 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-
+//Rota para registro
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
 
+//Rota para login
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-Route::middleware([\App\Http\Middleware\CheckTokenExpiry::class, 'auth:sanctum'])->group(function () { 
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/verify', [AuthController::class, 'verify']);
-});
- 
+//Rota para logout
+Route::post('/logout', [AuthController::class, 'logout']);
+
+//Rota para verificar se token é valido (pode tambem ser usada a middleware auth:sanctum, porém ela irá retornar 405 se for expirado)
+Route::get('/verify', [AuthController::class, 'verify'])->middleware(\App\Http\Middleware\CheckTokenExpiry::class);
